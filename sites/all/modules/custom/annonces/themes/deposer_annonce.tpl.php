19c456189e384615bab7e81ac7f23607c9036f19
bensmedamine <bens.medamine@gmail.com>
2012-10-26 20:12:19 +0000

    Signed-off-by: bensmedamine <bens.medamine@gmail.com>

--- sites/all/modules/custom/annonces/themes/deposer_annonce.tpl.php
+++ sites/all/modules/custom/annonces/themes/deposer_annonce.tpl.php
@@ -1,6 +1,6 @@
 
 <?php if (!user_is_logged_in()): ?>
-  <p class="fs-12"><strong class="blue">Seuls les membres authentifiés peuvent déposer une annonce !</strong></p>
+  <p class="acces-deny blue">Seuls les membres authentifiés peuvent déposer une annonce !</p>
   <p class="fs-12"><span class="blue">&rarr;</span> Vous avez déjà un compte? veuillez vous authentifier sur la page <a href="/mon-compte" title="Mon compte">Mon compte</a>.</p>
   <p class="fs-12"><span class="blue">&rarr;</span> <strong>Vous n’avez pas encore un compte ?</strong><br /> veuillez vous inscrire en seulement <strong>2</strong> minutes en suivant ce <a href="/inscription" title="Inscription">lien</a>.</p>
 
